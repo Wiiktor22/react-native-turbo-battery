ac8359ab60a1e9b7bd4b019aa47b467a6b2bfd25
Wiktor Szlegier <22wiktors@gmail.com>
2023-08-12 16:55:32 +0200

    fix: add spec

--- android/src/oldarch/TurboBatterySpec.kt
+++ android/src/oldarch/TurboBatterySpec.kt
@@ -3,6 +3,7 @@ package com.turbobattery
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.Callback
 
 abstract class TurboBatterySpec internal constructor(context: ReactApplicationContext) :
   ReactContextBaseJavaModule(context) {
@@ -11,5 +12,10 @@ abstract class TurboBatterySpec internal constructor(context: ReactApplicationCo
 
   abstract fun getBatteryLevel(promise: Promise)
 
+  abstract fun getBatteryLevelSync(): Double
+  
   abstract fun getBatteryState(promise: Promise)
+
+  abstract fun getLowPowerState(successCallback: Callback, errorCallback: Callback)
+
 }
